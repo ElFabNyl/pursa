0e0e798e08191bbc8119753e37a78a652eba1733
ElFabNyl <elfabnyl@gmail.com>
2022-07-19 13:10:38 +0100

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return MaterialApp(
+      home: Container(),
     );
   }
 }
