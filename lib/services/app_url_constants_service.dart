11187e845044380e5bf6a7946db0e9159414896a
ElFabNyl <elfabnyl@gmail.com>
2022-07-27 21:39:45 +0100

    auth

---
+++ lib/services/app_url_constants_service.dart
@@ -0,0 +1,9 @@
+class AppUrl {
+  static const Map<String, String> headers = {
+    "Content-Type": "application/json; charset=UTF-8",
+    'Accept': 'application/json',
+  };
+
+  static const String baseUrl = 'https://exchange.pursa.co/api/v1/';
+
+}
